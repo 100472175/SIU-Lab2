59450257d5e3dc854b11130fa7acdf32d2899aa5
Eduardo <100472175@alumnos.uc3m.es>
2024-03-21 11:03:35 +0100

    added the endpoint of the tasks

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
 const PORT = 3000;
 
 const serveStaticFile = async (file) => {
   return new Promise((resolve, reject) => {
-    fs.readFile(file, function(err, data) {
-      if(err) reject(err);
+    fs.readFile(file, function (err, data) {
+      if (err) reject(err);
       resolve(data);
     });
   });
-} 
+};
 
 const sendResponse = (response, content, contentType) => {
-  response.writeHead(200, {"Content-Type": contentType});
+  response.writeHead(200, { "Content-Type": contentType });
   response.end(content);
-}
+};
 
 const handleRequest = async (request, response) => {
   const url = request.url;
 
-  if(request.method === "GET"){
+  if (request.method === "GET") {
     let content;
     let contentType;
-    switch(url){
+    switch (url) {
       case "/":
       case "/index.html":
         content = await serveStaticFile("www/index.html");
@@ -37,18 +37,21 @@ const handleRequest = async (request, response) => {
         content = await serveStaticFile("www/style.css");
         contentType = "text/css";
         break;
-      default: 
+      case "/tasks/get":
+        content = await serveStaticFile("tasks.json");
+        contentType = "application/json";
+        break;
+      default:
         content = "Ruta no v&aacutelida\r\n";
         contentType = "text/html";
     }
 
-     sendResponse(response, content, contentType);
-  } else{
-     response.writeHead(405, {"Content-Type": "text/html"});
-     response.write(`M&eacutetodo ${request.method} no permitido!\r\n`);
+    sendResponse(response, content, contentType);
+  } else {
+    response.writeHead(405, { "Content-Type": "text/html" });
+    response.write(`M&eacutetodo ${request.method} no permitido!\r\n`);
   }
-}
-
+};
 
 const server = http.createServer(handleRequest);
 server.listen(PORT);
